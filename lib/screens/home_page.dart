ba5ccf732b34881698539ddc0c58ec9f74a2c331
Mario Aguilar <marioaguilarj@hotmail.com>
2025-10-09 16:40:00 -0600

    AddBarr added

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Icon(
+          Icons.menu,
+          color:Colors.grey[800]
+        ),
+        actions:[Padding(
+          padding: const EdgeInsets.only(right:24.0),
+          child: Icon(Icons.person),
+          ),
+        ],
+      ),
+      body: const Column(
+        children: [
+          //1. texto principal
+
+          //2. Pestañas
+
+          //3. Contenido de las pestañas
+
+          //4. Carrito
+        ],
+      ),
+    );
+  }
+}
